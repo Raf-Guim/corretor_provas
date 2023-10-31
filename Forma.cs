a1c429c7dd9d2bbcd600f37b07f6d253e12ce504
Rafael Guimaraes <raf.plguimaraes@gmail.com>
2023-10-31 11:26:08 -0300

    feat: Finalizacao do codigo

--- Forma.cs
+++ Forma.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Text;
 using SkiaSharp;
 
@@ -348,6 +349,21 @@ namespace Projeto
 		}
 	}
 
+	public class AreaValidacao
+	{
+		public int X0 { get; set; }
+		public int Y0 { get; set; }
+		public int X1 { get; set; }
+		public int Y1 { get; set; }
+
+		public AreaValidacao(int x0, int y0, int x1, int y1)
+		{
+			X0 = x0;
+			Y0 = y0;
+			X1 = x1;
+			Y1 = y1;
+		}
+	}
 	class Program
 	{
 
@@ -439,7 +455,7 @@ namespace Projeto
 
 		static unsafe byte* SemErosao(byte* invertido, byte* sem_erosao, int largura, int altura)
 		{
-			int tamanhoJanela = 9;
+			int tamanhoJanela = 7;
 			int metadeJanela = tamanhoJanela / 2;
 
 			for (int y = 0; y < altura; y++)
@@ -454,15 +470,64 @@ namespace Projeto
 			return sem_erosao;
 		}
 
+		static unsafe byte* RegularizaImagem(byte* entrada, int altura, int largura)
+		{
+			using (SKBitmap bitmapRegularizado = new SKBitmap(new SKImageInfo(largura, altura, SKColorType.Gray8)))
+			{
+				byte* cinza = (byte*)bitmapRegularizado.GetPixels();
+
+				int total_pixels_entrada = altura * largura;
+				int sum = 0;
+
+				cinza = ConverterCinza(entrada, cinza, total_pixels_entrada, out sum);
+
+				int media = sum / total_pixels_entrada;
+
+				byte* limiarizado = (byte*)bitmapRegularizado.GetPixels();
+				limiarizado = Limiarizacao(cinza, limiarizado, total_pixels_entrada, media);
+
+				byte* invertido = (byte*)bitmapRegularizado.GetPixels();
+				invertido = Invercao(limiarizado, invertido, total_pixels_entrada);
+
+				byte* sem_erosao = (byte*)bitmapRegularizado.GetPixels();
+				byte* regularizado = (byte*)bitmapRegularizado.GetPixels();
+				regularizado = SemErosao(invertido, sem_erosao, largura, altura);
+
+				return regularizado;
+			}
+		}
+		static bool ValidacaoProva(List<Forma> lista_formas, List<AreaValidacao> lista_areas_quadrados_validacao)
+		{
+			int quadrados_validacao_prova = 0;
+			for (int i = 0; i < lista_formas.Count; i++)
+			{
+				for (int j = 0; j < lista_areas_quadrados_validacao.Count; j++)
+				{
+					if (lista_formas[i].FazInterseccao(lista_areas_quadrados_validacao[j].X0, lista_areas_quadrados_validacao[j].Y0, lista_areas_quadrados_validacao[j].X1, lista_areas_quadrados_validacao[j].Y1))
+					{
+						quadrados_validacao_prova++;
+					}
+				}
+			}
+			// Console.WriteLine("Quadrados de validação: " + quadrados_validacao_prova);
+			if (quadrados_validacao_prova != 6)
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
 		static void Main(string[] args)
 		{
-			using (SKBitmap bitmapEntrada = SKBitmap.Decode("/Users/rafael/Documents/University/2023-02/computacao_cognitiva/atividade_grupo_1/gabarito_correto_1.png"),
+			using (SKBitmap bitmapEntrada = SKBitmap.Decode("/Users/rafael/Documents/University/2023-02/computacao_cognitiva/atividade_grupo_1/gabarito_correto_3.png"),
 				bitmapCinza = new SKBitmap(new SKImageInfo(bitmapEntrada.Width, bitmapEntrada.Height, SKColorType.Gray8)),
 				bitmapLimiarizado = new SKBitmap(new SKImageInfo(bitmapEntrada.Width, bitmapEntrada.Height, SKColorType.Gray8)),
 				bitmapInvertido = new SKBitmap(new SKImageInfo(bitmapEntrada.Width, bitmapEntrada.Height, SKColorType.Gray8)),
 				bitmapSemErosao = new SKBitmap(new SKImageInfo(bitmapEntrada.Width, bitmapEntrada.Height, SKColorType.Gray8)))
 			{
-
+				// Console.WriteLine(bitmapEntrada.ColorType);
 				unsafe
 				{
 					byte* entrada = (byte*)bitmapEntrada.GetPixels();
@@ -485,6 +550,138 @@ namespace Projeto
 					invertido = Invercao(limiarizado, invertido, total_pixels_entrada);
 					sem_erosao = SemErosao(invertido, sem_erosao, largura, altura);
 
+					// byte* regularizado = RegularizaImagem(entrada, altura, largura);
+
+					List<Forma> lista_formas = Forma.DetectarFormas(sem_erosao, largura, altura, true);
+
+					List<AreaValidacao> lista_areas_quadrados_validacao = new List<AreaValidacao>();
+					lista_areas_quadrados_validacao.Add(new AreaValidacao(30, 30, 84, 84));
+					lista_areas_quadrados_validacao.Add(new AreaValidacao(637, 30, 691, 84));
+					lista_areas_quadrados_validacao.Add(new AreaValidacao(30, 494, 84, 547));
+					lista_areas_quadrados_validacao.Add(new AreaValidacao(637, 494, 691, 547));
+					lista_areas_quadrados_validacao.Add(new AreaValidacao(30, 957, 84, 1011));
+					lista_areas_quadrados_validacao.Add(new AreaValidacao(637, 957, 691, 1011));
+
+					// Console.WriteLine("Formas encontradas: " + lista_formas.Count);
+
+					bool validacao_prova = ValidacaoProva(lista_formas, lista_areas_quadrados_validacao);
+
+					if (validacao_prova)
+					{
+						Console.WriteLine("Gabarito correto!");
+
+						Dictionary<int, List<String>> respostas = new Dictionary<int, List<String>>();
+						respostas.Add(0, new List<String>());
+						respostas.Add(1, new List<String>());
+						respostas.Add(2, new List<String>());
+						respostas.Add(3, new List<String>());
+						respostas.Add(4, new List<String>());
+						respostas.Add(5, new List<String>());
+						respostas.Add(6, new List<String>());
+						respostas.Add(7, new List<String>());
+						respostas.Add(8, new List<String>());
+						respostas.Add(9, new List<String>());
+
+
+						List<int> lista_posicao_x0 = new List<int>();
+						List<int> lista_posicao_y0 = new List<int>();
+						List<int> lista_posicao_x1 = new List<int>();
+						List<int> lista_posicao_y1 = new List<int>();
+
+
+						lista_posicao_x0.Add(207);
+						lista_posicao_x0.Add(272);
+						lista_posicao_x0.Add(337);
+						lista_posicao_x0.Add(402);
+						lista_posicao_x0.Add(468);
+
+
+						lista_posicao_y0.Add(330);
+						lista_posicao_y0.Add(394);
+						lista_posicao_y0.Add(459);
+						lista_posicao_y0.Add(524);
+						lista_posicao_y0.Add(588);
+						lista_posicao_y0.Add(653);
+						lista_posicao_y0.Add(718);
+						lista_posicao_y0.Add(782);
+						lista_posicao_y0.Add(847);
+						lista_posicao_y0.Add(912);
+
+
+						lista_posicao_x1.Add(251);
+						lista_posicao_x1.Add(316);
+						lista_posicao_x1.Add(381);
+						lista_posicao_x1.Add(447);
+						lista_posicao_x1.Add(512);
+
+
+						lista_posicao_y1.Add(374);
+						lista_posicao_y1.Add(439);
+						lista_posicao_y1.Add(503);
+						lista_posicao_y1.Add(568);
+						lista_posicao_y1.Add(633);
+						lista_posicao_y1.Add(697);
+						lista_posicao_y1.Add(762);
+						lista_posicao_y1.Add(827);
+						lista_posicao_y1.Add(891);
+						lista_posicao_y1.Add(956);
+
+						for (int forma_aux = 0; forma_aux < lista_formas.Count; forma_aux++)
+						{
+							for (int y_aux = 0; y_aux < 10; y_aux++)
+							{
+								for (int x_aux = 0; x_aux < 5; x_aux++)
+								{
+									if (x_aux == 0 && lista_formas[forma_aux].FazInterseccao(lista_posicao_x0[x_aux], lista_posicao_y0[y_aux], lista_posicao_x1[x_aux], lista_posicao_y1[y_aux]))
+									{
+										respostas[y_aux].Add("A");
+									}
+									else if (x_aux == 1 && lista_formas[forma_aux].FazInterseccao(lista_posicao_x0[x_aux], lista_posicao_y0[y_aux], lista_posicao_x1[x_aux], lista_posicao_y1[y_aux]))
+									{
+										respostas[y_aux].Add("B");
+									}
+									else if (x_aux == 2 && lista_formas[forma_aux].FazInterseccao(lista_posicao_x0[x_aux], lista_posicao_y0[y_aux], lista_posicao_x1[x_aux], lista_posicao_y1[y_aux]))
+									{
+										respostas[y_aux].Add("C");
+									}
+									else if (x_aux == 3 && lista_formas[forma_aux].FazInterseccao(lista_posicao_x0[x_aux], lista_posicao_y0[y_aux], lista_posicao_x1[x_aux], lista_posicao_y1[y_aux]))
+									{
+										respostas[y_aux].Add("D");
+									}
+									else if (x_aux == 4 && lista_formas[forma_aux].FazInterseccao(lista_posicao_x0[x_aux], lista_posicao_y0[y_aux], lista_posicao_x1[x_aux], lista_posicao_y1[y_aux]))
+									{
+										respostas[y_aux].Add("E");
+									}
+								}
+							}
+						}
+
+						for (int i = 0; i < 10; i++)
+						{
+							String resposta = "Questão " + (i + 1) + ": ";
+							if (respostas[i].Count == 0)
+							{
+								resposta += "Nenhuma alternativa";
+							}
+							else
+							{
+								for (int j = 0; j < respostas[i].Count; j++)
+								{
+									resposta += respostas[i][j];
+									if (j < (respostas[i].Count - 1))
+									{
+										resposta += ", ";
+									}
+								}
+
+							}
+							Console.WriteLine(resposta);
+						}
+					}
+					else
+					{
+						Console.WriteLine("Gabarito errado!");
+					}
 				}
 
 				using (FileStream stream = new FileStream("/Users/rafael/Documents/University/2023-02/computacao_cognitiva/atividade_grupo_1/gabarito_correto_1_cinza.png", FileMode.OpenOrCreate, FileAccess.Write))
--- README.md
+++ README.md
@@ -1,1 +1,81 @@
 # corretor_provas
+
+Formas encontradas: 56
+
+## Test validation squares
+- Area: 3025 / X0: 30 / Y0: 30 / X1: 84 / Y1: 84 / CentroX: 57 / CentroY: 57
+- Area: 3025 / X0: 637 / Y0: 30 / X1: 691 / Y1: 84 / CentroX: 664 / CentroY: 57
+- Area: 2970 / X0: 30 / Y0: 494 / X1: 84 / Y1: 547 / CentroX: 57 / CentroY: 520
+- Area: 2970 / X0: 637 / Y0: 494 / X1: 691 / Y1: 547 / CentroX: 664 / CentroY: 520
+- Area: 3025 / X0: 30 / Y0: 957 / X1: 84 / Y1: 1011 / CentroX: 57 / CentroY: 984
+- Area: 3025 / X0: 637 / Y0: 957 / X1: 691 / Y1: 1011 / CentroX: 664 / CentroY: 984
+
+## Question 1
+- Area: 301 / X0: 207 / Y0: 330 / X1: 251 / Y1: 374 / CentroX: 229 / CentroY: 352
+- Area: 302 / X0: 272 / Y0: 330 / X1: 316 / Y1: 374 / CentroX: 294 / CentroY: 352
+- Area: 302 / X0: 337 / Y0: 330 / X1: 381 / Y1: 374 / CentroX: 359 / CentroY: 352
+- Area: 302 / X0: 402 / Y0: 330 / X1: 447 / Y1: 374 / CentroX: 424 / CentroY: 352
+- Area: 302 / X0: 468 / Y0: 330 / X1: 512 / Y1: 374 / CentroX: 490 / CentroY: 352
+
+## Question 2
+- Area: 303 / X0: 207 / Y0: 394 / X1: 251 / Y1: 439 / CentroX: 229 / CentroY: 416
+- Area: 303 / X0: 272 / Y0: 394 / X1: 316 / Y1: 439 / CentroX: 294 / CentroY: 416
+- Area: 303 / X0: 337 / Y0: 394 / X1: 381 / Y1: 439 / CentroX: 359 / CentroY: 416
+- Area: 308 / X0: 402 / Y0: 394 / X1: 447 / Y1: 439 / CentroX: 424 / CentroY: 416
+- Area: 303 / X0: 468 / Y0: 394 / X1: 512 / Y1: 439 / CentroX: 490 / CentroY: 416
+
+## Question 3
+- Area: 302 / X0: 207 / Y0: 459 / X1: 251 / Y1: 503 / CentroX: 229 / CentroY: 481
+- Area: 302 / X0: 272 / Y0: 459 / X1: 316 / Y1: 503 / CentroX: 294 / CentroY: 481
+- Area: 302 / X0: 337 / Y0: 459 / X1: 381 / Y1: 503 / CentroX: 359 / CentroY: 481
+- Area: 306 / X0: 402 / Y0: 459 / X1: 447 / Y1: 503 / CentroX: 424 / CentroY: 481
+- Area: 302 / X0: 468 / Y0: 459 / X1: 512 / Y1: 503 / CentroX: 490 / CentroY: 481
+
+## Question 4
+- Area: 303 / X0: 207 / Y0: 524 / X1: 251 / Y1: 568 / CentroX: 229 / CentroY: 546
+- Area: 303 / X0: 272 / Y0: 524 / X1: 316 / Y1: 568 / CentroX: 294 / CentroY: 546
+- Area: 303 / X0: 337 / Y0: 524 / X1: 381 / Y1: 568 / CentroX: 359 / CentroY: 546
+- Area: 303 / X0: 402 / Y0: 524 / X1: 447 / Y1: 568 / CentroX: 424 / CentroY: 546
+- Area: 303 / X0: 468 / Y0: 524 / X1: 512 / Y1: 568 / CentroX: 490 / CentroY: 546
+
+## Question 5
+- Area: 303 / X0: 207 / Y0: 588 / X1: 251 / Y1: 633 / CentroX: 229 / CentroY: 610
+- Area: 303 / X0: 272 / Y0: 588 / X1: 316 / Y1: 633 / CentroX: 294 / CentroY: 610
+- Area: 303 / X0: 337 / Y0: 588 / X1: 381 / Y1: 633 / CentroX: 359 / CentroY: 610
+- Area: 308 / X0: 402 / Y0: 588 / X1: 447 / Y1: 633 / CentroX: 424 / CentroY: 610
+- Area: 303 / X0: 468 / Y0: 588 / X1: 512 / Y1: 633 / CentroX: 490 / CentroY: 610
+
+## Question 6
+- Area: 303 / X0: 207 / Y0: 653 / X1: 251 / Y1: 697 / CentroX: 229 / CentroY: 675
+- Area: 303 / X0: 272 / Y0: 653 / X1: 316 / Y1: 697 / CentroX: 294 / CentroY: 675
+- Area: 303 / X0: 337 / Y0: 653 / X1: 381 / Y1: 697 / CentroX: 359 / CentroY: 675
+- Area: 306 / X0: 402 / Y0: 653 / X1: 447 / Y1: 697 / CentroX: 424 / CentroY: 675
+- Area: 303 / X0: 468 / Y0: 653 / X1: 512 / Y1: 697 / CentroX: 490 / CentroY: 675
+
+## Question 7
+- Area: 303 / X0: 207 / Y0: 718 / X1: 251 / Y1: 762 / CentroX: 229 / CentroY: 740
+- Area: 303 / X0: 272 / Y0: 718 / X1: 316 / Y1: 762 / CentroX: 294 / CentroY: 740
+- Area: 303 / X0: 337 / Y0: 718 / X1: 381 / Y1: 762 / CentroX: 359 / CentroY: 740
+- Area: 304 / X0: 402 / Y0: 718 / X1: 447 / Y1: 762 / CentroX: 424 / CentroY: 740
+- Area: 303 / X0: 468 / Y0: 718 / X1: 512 / Y1: 762 / CentroX: 490 / CentroY: 740
+
+## Question 8
+- Area: 303 / X0: 207 / Y0: 782 / X1: 251 / Y1: 827 / CentroX: 229 / CentroY: 804
+- Area: 303 / X0: 272 / Y0: 782 / X1: 316 / Y1: 827 / CentroX: 294 / CentroY: 804
+- Area: 303 / X0: 337 / Y0: 782 / X1: 381 / Y1: 827 / CentroX: 359 / CentroY: 804
+- Area: 308 / X0: 402 / Y0: 782 / X1: 447 / Y1: 827 / CentroX: 424 / CentroY: 804
+- Area: 303 / X0: 468 / Y0: 782 / X1: 512 / Y1: 827 / CentroX: 490 / CentroY: 804
+
+## Question 9
+- Area: 303 / X0: 207 / Y0: 847 / X1: 251 / Y1: 891 / CentroX: 229 / CentroY: 869
+- Area: 303 / X0: 272 / Y0: 847 / X1: 316 / Y1: 891 / CentroX: 294 / CentroY: 869
+- Area: 303 / X0: 337 / Y0: 847 / X1: 381 / Y1: 891 / CentroX: 359 / CentroY: 869
+- Area: 306 / X0: 402 / Y0: 847 / X1: 447 / Y1: 891 / CentroX: 424 / CentroY: 869
+- Area: 303 / X0: 468 / Y0: 847 / X1: 512 / Y1: 891 / CentroX: 490 / CentroY: 869
+
+## Question 10
+- Area: 303 / X0: 207 / Y0: 912 / X1: 251 / Y1: 956 / CentroX: 229 / CentroY: 934
+- Area: 303 / X0: 272 / Y0: 912 / X1: 316 / Y1: 956 / CentroX: 294 / CentroY: 934
+- Area: 303 / X0: 337 / Y0: 912 / X1: 381 / Y1: 956 / CentroX: 359 / CentroY: 934
+- Area: 304 / X0: 402 / Y0: 912 / X1: 447 / Y1: 956 / CentroX: 424 / CentroY: 934
+- Area: 303 / X0: 468 / Y0: 912 / X1: 512 / Y1: 956 / CentroX: 490 / CentroY: 934
